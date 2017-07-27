074e0ef46cf5a094d755d5aec206c7764c68a174
frankw <fwessels@xs4all.nl>
2017-07-27 10:19:47 -0700

    Initial commit

---
+++ HashCompare.go
@@ -0,0 +1,267 @@
+package main
+
+import (
+	"container/heap"
+	"encoding/hex"
+	"fmt"
+	"github.com/aead/siphash"
+	"github.com/aead/poly1305"
+	"github.com/minio/blake2b-simd"
+	"github.com/dgryski/go-highway"
+	"math/big"
+	"math/rand"
+	"runtime"
+	"sort"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func Poly1305Sum(buf []byte, key [32]byte) []byte {
+	h := poly1305.New(key)
+	h.Write(buf[:])
+	sum := h.Sum(nil)
+	return sum
+}
+
+func Blake2bSum(buf []byte) []byte {
+	h := blake2b.New512()
+	h.Reset()
+	h.Write(buf[:])
+	sum := h.Sum(nil)
+	return sum
+}
+
+func Blake2b256Sum(buf []byte) []byte {
+	h := blake2b.New256()
+	h.Reset()
+	h.Write(buf[:])
+	sum := h.Sum(nil)
+	return sum
+}
+
+func SipHashSum(buf []byte, key [32]byte) []byte {
+	h, _ := siphash.New128(key[0:16])
+	h.Reset()
+	h.Write(buf[:])
+	sum := h.Sum(nil)
+	return sum
+}
+
+func PutUint(buf []byte, x uint64) int {
+	for i := 0; i < 8; i++ {
+		buf[i] = byte(x)
+		x >>= 8
+	}
+	return 8
+}
+
+func HighwayHash(buf []byte) []byte {
+	key := highway.Lanes{0x0706050403020100, 0x0F0E0D0C0B0A0908, 0x1716151413121110, 0x1F1E1D1C1B1A1918}
+	h := highway.Hash(key, buf[:])
+	b := make([]byte, 8)
+	PutUint(b, h)
+	return b
+}
+
+func mask(b, m int) byte {
+	s := uint(9 - m)
+	return byte(((1 << s) - 1) << uint(b%m))
+}
+
+// Slice of sorted bytes
+type SortBytes [][]byte
+
+func (s SortBytes) Less(i, j int) bool {
+	for index := range s[i] {
+		if s[i][index] != s[j][index] {
+			return s[i][index] < s[j][index]
+		}
+	}
+	return false
+}
+
+func (s SortBytes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s SortBytes) Len() int {
+	return len(s)
+}
+
+func TestHashPermutationsRange(msg []byte, key [32]byte, cpu int, cpuShift uint, algo string, results chan<- SortBytes) {
+
+	var keys SortBytes
+
+	for m := 8; m >= 1; m-- {
+
+		bStart := (len(msg) * m * cpu) >> cpuShift
+		bEnd := (len(msg) * m * (cpu + 1)) >> cpuShift
+
+		for b := bStart; b < bEnd; b++ {
+			// Change message with mask
+			msg[b/m] = msg[b/m] ^ mask(b, m)
+
+			var tag []byte
+			switch algo {
+			case "poly1305":
+				tag = Poly1305Sum(msg, key)
+			case "blake2b":
+				tag = Blake2bSum(msg)
+			case "blake2b-256":
+				tag = Blake2b256Sum(msg)
+			case "siphash":
+				tag = SipHashSum(msg, key)
+			case "highwayhash":
+				tag = HighwayHash(msg)
+			}
+
+			keys = append(keys, tag)
+
+			// Undo change
+			msg[b/m] = msg[b/m] ^ mask(b, m)
+		}
+	}
+
+	// sanity check if message not corrupted
+	for i := range msg {
+		if msg[i] != byte(i) {
+			panic("memory corrupted")
+		}
+	}
+
+	sort.Sort(keys)
+
+	results <- keys
+}
+
+func TestHashPermutations(key [32]byte, msgSize uint, algo string) (permutations, equalBits int, elapsed time.Duration) {
+	start := time.Now()
+
+	msg := make([]byte, msgSize)
+	for i := range msg {
+		msg[i] = byte(i)
+	}
+
+	cpus := runtime.NumCPU()
+	// make sure nr of CPUs is a power of 2
+	cpuShift := uint(len(strconv.FormatInt(int64(cpus), 2)) - 1)
+	cpus = 1 << cpuShift
+
+	var wg sync.WaitGroup
+	results := make(chan SortBytes)
+
+	for cpu := 0; cpu < cpus; cpu++ {
+
+		wg.Add(1)
+		go func(cpu int) {
+			msgCopy := make([]byte, len(msg))
+			copy(msgCopy, msg)
+			defer wg.Done()
+			TestHashPermutationsRange(msgCopy, key, cpu, cpuShift, algo, results)
+		}(cpu)
+	}
+
+	// Wait for workers to complete
+	go func() {
+		wg.Wait()
+		close(results) // Close output channel
+	}()
+
+	keys := make([]SortBytes, 0, cpus)
+
+	// Collect sub sorts
+	for r := range results {
+		keys = append(keys, r)
+	}
+
+	var tagprev *big.Int
+	var smallest *big.Int
+
+	h := &HexHeap{}
+	heap.Init(h)
+
+	// Push initial keys onto heap
+	for stack, sb := range keys {
+		if len(sb) > 0 {
+			heap.Push(h, Hex{hex.EncodeToString(sb[0]), stack, 1})
+		}
+	}
+
+	// Iterate over heap
+	for h.Len() > 0 {
+		hi := heap.Pop(h).(Hex)
+
+		tag := new(big.Int)
+		tag.SetString(hi.hex, 16)
+
+		if tagprev != nil {
+			diff := new(big.Int)
+			diff.Sub(tag, tagprev)
+			//fmt.Println(k, diff)
+
+			if smallest == nil {
+				smallest = new(big.Int)
+				*smallest = *diff
+			} else if smallest.Cmp(diff) > 0 {
+				*smallest = *diff
+			}
+		}
+
+		tagprev = tag
+
+		if hi.index < len(keys[hi.stack]) { // Push new entry when still available
+			heap.Push(h, Hex{hex.EncodeToString(keys[hi.stack][hi.index]), hi.stack, hi.index + 1})
+		}
+	}
+
+	elapsed = time.Since(start)
+	fmt.Println(smallest)
+	return len(keys)*len(keys[0]), 8*len(keys[0][0])-len(smallest.Text(2)), elapsed
+}
+
+// An HexHeap is a min-heap of hexadecimals.
+type HexHeap []Hex
+
+type Hex struct {
+	hex   string
+	stack int
+	index int
+}
+
+func (h HexHeap) Len() int           { return len(h) }
+func (h HexHeap) Less(i, j int) bool { return h[i].hex < h[j].hex }
+func (h HexHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *HexHeap) Push(x interface{}) {
+	*h = append(*h, x.(Hex))
+}
+
+func (h *HexHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	var key [32]byte
+	for i := range key {
+		key[i] = byte(255 - i)
+	}
+
+	algo := ""
+	//algo = "blake2b"
+	//algo = "blake2b-256"
+	//algo = "poly1305"
+	//algo = "siphash"
+	algo = "highwayhash"
+	for shift := uint(8); shift < 14; shift++ {
+		permutations, equalBits, elapsed := TestHashPermutations(key, 1<<shift, algo)
+		fmt.Printf("Permutations: %d -- equal bits: %d -- duration: %v (%s)\n", permutations, equalBits, elapsed, algo)
+
+	}
+}
